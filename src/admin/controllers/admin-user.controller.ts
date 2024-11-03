18e7a41b70db66f04c892f86da2a9769f2601f68
allenlin <ht016746@gmail.com>
2024-11-03 10:51:10 +0700

    chore: apply guards to admin user controller

--- src/admin/controllers/admin-user.controller.ts
+++ src/admin/controllers/admin-user.controller.ts
@@ -6,12 +6,12 @@ import { Serialize } from '@/interceptors/serialize.interceptor';
 import { ResetTokenResponseDto } from '../dtos/reset-token-response.dto';
 import { AdminService } from '../admin.service';
 
+@UseGuards(AuthGuard, AdminGuard)
 @Controller('admin/users')
 export class AdminUsersController {
   constructor(private adminService: AdminService) {}
 
   @Serialize(ResetTokenResponseDto)
-  @UseGuards(AuthGuard, AdminGuard)
   @Post(':user_id/reset-token')
   async createUserResetToken(@Param('user_id') userId: string) {
     return this.adminService.generateResetToken(userId);
